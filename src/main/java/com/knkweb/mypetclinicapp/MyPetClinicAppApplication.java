b1d4c579345a90928ea2550d80314165437870a7
unknown <nandakumarkaliappan@gmail.com>
2023-07-19 10:00:36 +0530

    Converted to maven project
    added necessary files

---
+++ src/main/java/com/knkweb/mypetclinicapp/MyPetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.knkweb.mypetclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetClinicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knkweb/mypetclinicapp/MyPetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knkweb.mypetclinicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
