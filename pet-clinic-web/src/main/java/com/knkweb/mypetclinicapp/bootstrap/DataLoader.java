b42f9bf53c8cdcb9e1891957859c9407a5488b36
unknown <nandakumarkaliappan@gmail.com>
2023-07-25 20:08:25 +0530

    created bootstrap data closes #14

---
+++ pet-clinic-web/src/main/java/com/knkweb/mypetclinicapp/bootstrap/DataLoader.java
@@ -0,0 +1,47 @@
+package com.knkweb.mypetclinicapp.bootstrap;
+
+import com.knkweb.mypetclinicapp.model.Owner;
+import com.knkweb.mypetclinicapp.model.Vet;
+import com.knkweb.mypetclinicapp.services.OwnerService;
+import com.knkweb.mypetclinicapp.services.VetService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Ram");
+        owner1.setLastName("kumar");
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Arav");
+        owner2.setLastName("sha");
+        System.out.println("Owners loaded");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Durai");
+        vet1.setLastName("ravi");
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Sam");
+        vet2.setLastName("vo");
+        System.out.println("Vets loaded");
+
+    }
+
+
+}
