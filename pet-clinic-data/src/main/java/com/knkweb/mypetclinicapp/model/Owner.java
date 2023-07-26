6b531c3c4de61a3c727fe706539939e48ffdb71b
unknown <nandakumarkaliappan@gmail.com>
2023-07-26 15:35:28 +0530

    Created Speciality class, closes #27

--- pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/Owner.java
+++ pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/Owner.java
@@ -4,5 +4,5 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person{
-    private Set<Pet> pets = new HashSet<>(id);
+    private Set<Pet> pets = new HashSet<>();
 }
