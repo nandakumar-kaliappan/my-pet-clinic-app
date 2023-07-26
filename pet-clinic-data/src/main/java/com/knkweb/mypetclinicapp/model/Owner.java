bd12c9ae7741b1d2665c8219af41ba8089ecd7e0
unknown <nandakumarkaliappan@gmail.com>
2023-07-26 15:28:54 +0530

    Created Visit entity, closes #26

--- pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/Owner.java
+++ pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/Owner.java
@@ -1,5 +1,8 @@
 package com.knkweb.mypetclinicapp.model;
 
-public class Owner extends Person{
+import java.util.HashSet;
+import java.util.Set;
 
+public class Owner extends Person{
+    private Set<Pet> pets = new HashSet<>(id);
 }
