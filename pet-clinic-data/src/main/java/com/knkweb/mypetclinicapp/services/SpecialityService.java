d33a6d70207e9377876fb660292b3cdabbb67c58
unknown <nandakumarkaliappan@gmail.com>
2023-07-26 19:14:41 +0530

    Added Spetiality service and map impl, Closes #31

---
+++ pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.knkweb.mypetclinicapp.services;
+
+import com.knkweb.mypetclinicapp.model.Speciality;
+import com.knkweb.mypetclinicapp.services.CrudService;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.knkweb.mypetclinicapp.services.map;
+
+import com.knkweb.mypetclinicapp.model.Speciality;
+import com.knkweb.mypetclinicapp.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
