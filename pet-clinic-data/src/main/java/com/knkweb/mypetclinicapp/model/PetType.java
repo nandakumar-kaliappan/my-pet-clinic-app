87f2f5d2da2b891c550bad4c8452c326f6d27f6f
nandakumar-kaliappan <nandakumarkalliappan@gmail.com>
2023-08-01 22:29:08 +0530

    Converted owner to entity closes #36

--- pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/PetType.java
+++ pet-clinic-data/src/main/java/com/knkweb/mypetclinicapp/model/PetType.java
@@ -1,6 +1,14 @@
 package com.knkweb.mypetclinicapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pettypes")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
 
     public PetType() {
